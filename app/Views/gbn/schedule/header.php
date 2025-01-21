e55b7a68459b4eb72c7a810042b0a1609a4d5226
ydwhf <yudawhf@gmail.com>
2025-01-21 16:52:45 +0700

    crud data mesin

--- app/Views/gbn/schedule/header.php
+++ app/Views/gbn/schedule/header.php
@@ -13,7 +13,7 @@
 
                 <ul class="navbar-nav  justify-content-end">
                     <li class="nav-item d-flex align-items-center px-1">
-                        <a href="<?= base_url($role . '/schedule/mesinCelup') ?>">
+                        <a href="<?= base_url($role . '/mesin/mesinCelup') ?>">
                             <span class=" badge bg-gradient-info ">Data Mesin</span>
 
                         </a>
