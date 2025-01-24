5b9952c68d4cbece4db8b062ae231414c8856cb8
shabira782 <shabiranurul7@gmail.com>
2025-01-24 16:02:11 +0700

    add kolom ukuran di master

--- app/Views/celup/layout.php
+++ app/Views/celup/layout.php
@@ -110,7 +110,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link  " href="<?= base_url($role . '/pemesanan') ?>">
+                    <a class="nav-link  " href="<?= base_url($role . '/outCelup') ?>">
                         <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <svg width="12px" height="12px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                                 <title>document</title>
@@ -132,7 +132,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link  " href="<?= base_url($role . '/poplus') ?>">
+                    <a class="nav-link  " href="<?= base_url($role . '/retur') ?>">
                         <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <svg width="12px" height="12px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                                 <title>document</title>
