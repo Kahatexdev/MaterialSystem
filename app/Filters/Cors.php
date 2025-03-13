90a0fafeb4dacfe5d147b23b23d10a054acd95ae
Shabira782 <shabiranurul7@gmail.com>
2025-03-13 16:42:09 +0700

    cors

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -25,32 +25,12 @@ class Cors implements FilterInterface
      */
     public function before(RequestInterface $request, $arguments = null)
     {
-        namespace App\Filters;
-
-use CodeIgniter\HTTP\RequestInterface;
-use CodeIgniter\HTTP\ResponseInterface;
-use CodeIgniter\Filters\FilterInterface;
-
-class Cors implements FilterInterface
-{
-    public function before(RequestInterface $request, $arguments = null)
-    {
-        $allowedOrigins = [
-            'http://localhost:8080', // Contoh: domain frontend
-            'http://172.23.44.14',  // Tambahkan IP frontend jika perlu
-        ];
-
-        $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
-        if (in_array($origin, $allowedOrigins)) {
-            header("Access-Control-Allow-Origin: $origin");
-            header("Access-Control-Allow-Credentials: true");
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-            header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-        }
-
-        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-            exit(0); // Berhenti di sini untuk preflight request
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == "OPTIONS") {
+            die();
         }
     }
 
