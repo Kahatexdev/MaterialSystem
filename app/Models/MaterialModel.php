d04e133081ddf82b19dfa7e6cd6b98b6c3e5d805
notfoundra <novandram15@gmail.com>
2025-03-14 11:28:49 +0700

    kg puish

--- app/Models/MaterialModel.php
+++ app/Models/MaterialModel.php
@@ -185,6 +185,7 @@ class MaterialModel extends Model
             material.composition, 
             material.gw, 
             material.qty_pcs, 
+            material.kgs, 
             material.loss, 
             SUM(material.kgs) AS ttl_kebutuhan
         ')
@@ -205,16 +206,5 @@ class MaterialModel extends Model
     ');
 
         return $builder->findAll();
-    }
-    public function getMaterialForPemesanan($model, $styleSize, $area)
-    {
-        return $this->select('master_material.jenis, material.*')
-            ->join('master_order', 'master_order.id_order=material.id_order')
-            ->join('master_material', 'master_material.item_type=material.item_type')
-            ->where('master_order.no_model', $model)
-            ->where('material.style_size', $styleSize)
-            ->where('material.area', $area)
-            ->orderBy('master_material.jenis, material.item_type', 'ASC')
-            ->findAll();
     }
 }
