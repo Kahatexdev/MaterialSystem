f6702a31030ff6bb5ed6dddf03a197df101a032b
notfoundra <novandram15@gmail.com>
2025-03-17 09:36:02 +0700

    fix pemesanan

--- app/Controllers/PemesananController.php
+++ app/Controllers/PemesananController.php
@@ -73,8 +73,10 @@ class PemesananController extends BaseController
     }
     public function index()
     {
-        $area = $this->materialModel->getDataArea();
+        $apiUrl  = 'http://172.23.44.14/CapacityApps/public/api/getDataArea';
+        $response = file_get_contents($apiUrl);
 
+        $area = json_decode($response, true);
         $data = [
             'active' => $this->active,
             'title' => 'Material System',
