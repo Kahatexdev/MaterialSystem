d1ba9826825e1a71d107b3d81afefc08ac1f3ec5
notfoundra <novandram15@gmail.com>
2025-04-29 12:46:13 +0700

    fix pengeluaran model

--- app/Models/PengeluaranModel.php
+++ app/Models/PengeluaranModel.php
@@ -74,13 +74,13 @@ class PengeluaranModel extends Model
     public function searchPengiriman($noModel)
     {
         return $this->db->table('pengeluaran')
-            ->select('pengeluaran.*, out_celup.lot_kirim, schedule_celup.no_model, schedule_celup.kode_warna, schedule_celup.warna, schedule_celup.item_type')
+            ->select('pengeluaran.*, SUM(pengeluaran.kgs_out) AS kgs_out, out_celup.lot_kirim, schedule_celup.no_model, schedule_celup.kode_warna, schedule_celup.warna, schedule_celup.item_type')
             ->join('out_celup', 'out_celup.id_out_celup = pengeluaran.id_out_celup')
             ->join('bon_celup', 'bon_celup.id_bon = out_celup.id_bon')
             ->join('schedule_celup', 'schedule_celup.id_bon = bon_celup.id_bon')
             ->where('schedule_celup.no_model', $noModel)
             ->where('pengeluaran.status', 'Pengiriman Area')
-            ->distinct()
+            ->groupBy('schedule_celup.no_model, schedule_celup.kode_warna, schedule_celup.warna, schedule_celup.item_type')
             ->get()
             ->getResultArray();
     }
