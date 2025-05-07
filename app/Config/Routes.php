1a0b471654f3e980ed8b63d0b50ae930c42b2214
aileeensky <alinkhoiryra13@gmail.com>
2025-05-07 14:04:53 +0700

    Api Po Tambahan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -486,6 +486,6 @@ $routes->group(
         $routes->get('getTotalPengiriman', 'ApiController::getTotalPengiriman');
         $routes->post('warehouse/search', 'WarehouseController::search');
         $routes->get('warehouse/exportExcel', 'ExcelController::excelStockMaterial');
-        $routes->get('poTambahanDetail/(:any)', 'ApiController::poTambahanDetail/$1');
+        $routes->get('poTambahanDetail/(:any)/(:any)', 'ApiController::poTambahanDetail/$1/$2');
     }
 );
