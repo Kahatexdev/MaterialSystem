0c6890af77c3b82e5e1591efa9b18c6eac332496
notfoundra <novandram15@gmail.com>
2025-05-09 13:34:29 +0700

    lsit retur

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -492,6 +492,6 @@ $routes->group(
         $routes->get('poTambahanDetail/(:any)/(:any)', 'ApiController::poTambahanDetail/$1/$2');
         $routes->post('savePoTambahan', 'ApiController::savePoTambahan');
         $routes->get('cekMaterial/(:any)', 'ApiController::cekMaterial/$1');
-        $routes->get('listRetur', 'ApiController::listRetur');
+        $routes->post('listRetur', 'ApiController::listRetur');
     }
 );
