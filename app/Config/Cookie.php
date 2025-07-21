a52e8ad1a8185f1ca0d1b184836e98f95077ff47
Alfast456 <alfast456@gmail.com>
2025-07-21 13:45:36 +0700

    fix session

--- app/Config/Cookie.php
+++ app/Config/Cookie.php
@@ -36,7 +36,7 @@ class Cookie extends BaseConfig
      *
      * Typically will be a forward slash.
      */
-    public string $path = '/MaterialSystem/public/';
+    public string $path = '/';
 
     /**
      * --------------------------------------------------------------------------
@@ -45,7 +45,7 @@ class Cookie extends BaseConfig
      *
      * Set to `.your-domain.com` for site-wide cookies.
      */
-    public string $domain = 'http://172.23.44.14/MaterialSystem/public/';
+    public string $domain = '';
 
     /**
      * --------------------------------------------------------------------------
