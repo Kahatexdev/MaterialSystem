55fe78429020d93935a74f8ddb95a12197ba58b7
Alfast456 <alfast456@gmail.com>
2025-07-26 09:04:13 +0700

    import MU without API CapacityApps

--- app/Controllers/MasterdataController.php
+++ app/Controllers/MasterdataController.php
@@ -201,7 +201,7 @@ class MasterdataController extends BaseController
                 } elseif (strpos($validate['area'], 'KK') !== false) {
                     $unit = 'CIJERAH';
                 } else {
-                    $unit = '-';
+                    $unit = '';
                 }
 
                 // if (!$validate) {
