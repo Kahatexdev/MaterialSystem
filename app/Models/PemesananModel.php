2330c956edd4fa51a8cadd60683a6f48858c3ffc
Shabira782 <shabiranurul7@gmail.com>
2025-09-15 09:03:01 +0700

    kirim pemesanan

--- app/Models/PemesananModel.php
+++ app/Models/PemesananModel.php
@@ -283,6 +283,7 @@ class PemesananModel extends Model
             ->where('material.color', $id['color'])
             ->where('pemesanan.tgl_pakai', $id['tgl_pakai'])
             ->where('pemesanan.po_tambahan', $id['po_tambahan'])
+            ->where('pemesanan.admin', $id['area'])
             ->where('pemesanan.status_kirim!=', 'YA')
             ->get()
             ->getResultArray(); // Ambil semua baris sebagai array
