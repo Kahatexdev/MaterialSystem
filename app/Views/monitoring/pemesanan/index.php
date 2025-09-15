37bc0a33d0d9f3fc4dbe1c750b788fb41596ba34
notfoundra <novandram15@gmail.com>
2025-09-15 15:14:59 +0700

    index

--- app/Views/monitoring/pemesanan/index.php
+++ app/Views/monitoring/pemesanan/index.php
@@ -246,18 +246,24 @@
                                         <button type="button" class="btn btn-warning update-btn" data-bs-toggle="modal" data-bs-target="#updateListModal" data-area="<?= $id['admin']; ?>" data-tgl="<?= $id['tgl_pakai']; ?>" data-model="<?= $id['no_model']; ?>" data-item="<?= $id['item_type']; ?>" data-kode="<?= $id['kode_warna']; ?>" data-color="<?= $id['color']; ?>" data-po-tambahan="<?= $id['po_tambahan']; ?>">
                                             <i class="fa fa-edit fa-lg"></i>
                                         </button>
-                                        <button type="button" id="sendBtn"
-                                            class="btn btn-info text-xs send-btn"
-                                            data-toggle="modal"
-                                            data-area="<?= $id['no_model']; ?>"
-                                            data-tgl="<?= $id['tgl_pakai']; ?>"
-                                            data-model="<?= $id['no_model']; ?>"
-                                            data-item="<?= $id['item_type']; ?>"
-                                            data-kode="<?= $id['kode_warna']; ?>"
-                                            data-color="<?= $id['color']; ?>"
-                                            data-po-tambahan="<?= $id['po_tambahan']; ?>">
-                                            <i class="fa fa-paper-plane fa-lg"></i>
-                                        </button>
+                                        <?php if ($id['status_kirim'] != 'YA') { ?>
+                                            <button type="button" id="sendBtn"
+                                                class="btn btn-info text-xs send-btn"
+                                                data-toggle="modal"
+                                                data-area="<?= $id['no_model']; ?>"
+                                                data-tgl="<?= $id['tgl_pakai']; ?>"
+                                                data-model="<?= $id['no_model']; ?>"
+                                                data-item="<?= $id['item_type']; ?>"
+                                                data-kode="<?= $id['kode_warna']; ?>"
+                                                data-color="<?= $id['color']; ?>"
+                                                data-po-tambahan="<?= $id['po_tambahan']; ?>">
+                                                <i class="fa fa-paper-plane fa-lg"></i>
+                                            </button>
+
+                                        <?php } else { ?>
+                                            <span style="color: red;"></span>
+                                        <?php } ?>
+
                                     </td>
 
                                 </tr>
