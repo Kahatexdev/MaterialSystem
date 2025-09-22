a27b007b90859a0fc6392b98a2be91d8152c073a
aileeensky <alinkhoiryra13@gmail.com>
2025-09-22 09:07:00 +0700

    Tambahin sisa jatah di po tambahan

--- app/Models/PoTambahanModel.php
+++ app/Models/PoTambahanModel.php
@@ -87,7 +87,7 @@ class PoTambahanModel extends Model
     }
     public function getData()
     {
-        return $this->select('po_tambahan.id_po_tambahan, master_order.no_model, material.item_type, material.kode_warna, material.color, total_potambahan.ttl_tambahan_kg AS kg_poplus, total_potambahan.ttl_tambahan_cns AS cns_poplus, po_tambahan.status, DATE(po_tambahan.created_at) AS tgl_poplus, po_tambahan.admin, master_material.jenis')
+        return $this->select('po_tambahan.id_po_tambahan, master_order.no_model, material.item_type, material.kode_warna, material.color, total_potambahan.ttl_tambahan_kg AS kg_poplus, total_potambahan.ttl_tambahan_cns AS cns_poplus, total_potambahan.ttl_sisa_jatah AS sisa_jatah, po_tambahan.status, DATE(po_tambahan.created_at) AS tgl_poplus, po_tambahan.admin, master_material.jenis')
             ->join('material', 'po_tambahan.id_material = material.id_material', 'left')
             ->join('total_potambahan', 'po_tambahan.id_total_potambahan = total_potambahan.id_total_potambahan', 'left')
             ->join('master_order', 'material.id_order = master_order.id_order', 'left')
--- app/Views/monitoring/poplus/index.php
+++ app/Views/monitoring/poplus/index.php
@@ -88,6 +88,7 @@
                             <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder">Item Type</th>
                             <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder">Kode Warna</th>
                             <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder">Warna</th>
+                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder">Sisa Jatah</th>
                             <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder">Kg Po Tambahan</th>
                             <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder">Cns Po Tambahan</th>
                             <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder">Action</th>
@@ -105,6 +106,7 @@
                                 <td style="<?= $isNotApproved ? 'color:red;' : '' ?>"><?= $data['item_type'] ?></td>
                                 <td style="<?= $isNotApproved ? 'color:red;' : '' ?>"><?= $data['kode_warna'] ?></td>
                                 <td style="<?= $isNotApproved ? 'color:red;' : '' ?>"><?= $data['color'] ?></td>
+                                <td style="<?= $isNotApproved ? 'color:red;' : '' ?>"><?= number_format($data['sisa_jatah'], 2) ?></td>
                                 <td style="<?= $isNotApproved ? 'color:red;' : '' ?>"><?= number_format($data['kg_poplus'], 2) ?></td>
                                 <td style="<?= $isNotApproved ? 'color:red;' : '' ?>"><?= $data['cns_poplus'] ?></td>
                                 <td style="<?= $isNotApproved ? 'color:red;' : '' ?>">
