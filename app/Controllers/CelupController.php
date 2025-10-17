a6c7f1efd6dc7eea400f516017424683b5def8a3
Shabira782 <shabiranurul7@gmail.com>
2025-10-17 16:34:15 +0700

    Fix Controller generateBarcode Yuda

--- app/Controllers/CelupController.php
+++ app/Controllers/CelupController.php
@@ -831,7 +831,7 @@ class CelupController extends BaseController
         // Hasilkan barcode untuk setiap ID outCelup di grup
         foreach ($groupedDetails as &$group) {
             // dd($group);
-            $getDeskripsi = $this->masterMaterialModel->where('item_type', $group['item_type'])->select('deskripsi')->first();
+            $getDeskripsi = $this->masterMaterialModel->where('item_type', $detail['item_type'])->select('deskripsi')->first();
             if (!$getDeskripsi || empty($getDeskripsi['deskripsi'] ?? '')) {
                 $missing[] = $group['item_type'];
             }
