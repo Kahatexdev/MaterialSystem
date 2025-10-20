50c38dec127cd6a7a01d094d5635ad3495f814da
ydwhf <yudawhf@gmail.com>
2025-10-20 15:03:12 +0700

    Fix Model Datang Benang

--- app/Models/PemasukanModel.php
+++ app/Models/PemasukanModel.php
@@ -187,22 +187,22 @@ class PemasukanModel extends Model
             ->getCompiledSelect();
         // Query 1: Untuk data yang punya id_other_bon (other_bon)
         $builder1 = $this->db->table('pemasukan')
-            ->select('out_celup.id_out_celup, other_bon.id_other_bon, out_celup.id_other_bon, out_celup.l_m_d, out_celup.harga, out_celup.no_karung, SUM(DISTINCT out_celup.kgs_kirim) AS kgs_kirim, SUM(DISTINCT out_celup.cones_kirim) AS cones_kirim, SUM(DISTINCT out_celup.gw_kirim) AS gw_kirim,, out_celup.lot_kirim, other_bon.no_model, other_bon.item_type, other_bon.kode_warna, other_bon.warna, other_bon.no_surat_jalan, other_bon.tgl_datang, master_order.foll_up, master_order.no_order, master_order.buyer, master_order.delivery_awal, master_order.delivery_akhir, master_order.unit, m.total_kgs AS kgs_material, pemasukan.tgl_masuk, pemasukan.nama_cluster, other_bon.keterangan, pemasukan.admin, other_bon.po_tambahan AS po_plus')
+            ->select('out_celup.id_out_celup, other_bon.id_other_bon, out_celup.id_other_bon, out_celup.l_m_d, out_celup.harga, out_celup.no_karung, SUM(DISTINCT out_celup.kgs_kirim) AS kgs_kirim, SUM(DISTINCT out_celup.cones_kirim) AS cones_kirim, SUM(DISTINCT out_celup.gw_kirim) AS gw_kirim,, out_celup.lot_kirim, out_celup.no_model, other_bon.item_type, other_bon.kode_warna, other_bon.warna, other_bon.no_surat_jalan, other_bon.tgl_datang, master_order.foll_up, master_order.no_order, master_order.buyer, master_order.delivery_awal, master_order.delivery_akhir, master_order.unit, m.total_kgs AS kgs_material, pemasukan.tgl_masuk, pemasukan.nama_cluster, other_bon.keterangan, pemasukan.admin, other_bon.po_tambahan AS po_plus')
             ->join('out_celup', 'out_celup.id_out_celup = pemasukan.id_out_celup')
             ->join('other_bon', 'other_bon.id_other_bon = out_celup.id_other_bon')
-            ->join('master_order', 'master_order.no_model = other_bon.no_model', 'left')
-            ->join('open_po', 'open_po.no_model = other_bon.no_model AND open_po.kode_warna = other_bon.kode_warna AND open_po.item_type = other_bon.item_type', 'left')
-            ->join("($subMaterial) m", "m.no_model  = other_bon.no_model AND m.item_type  = other_bon.item_type AND m.kode_warna = other_bon.kode_warna AND m.color = other_bon.warna", 'left')
+            ->join('master_order', 'master_order.no_model = out_celup.no_model', 'left')
+            ->join('open_po', 'open_po.no_model = out_celup.no_model AND open_po.kode_warna = other_bon.kode_warna AND open_po.item_type = other_bon.item_type', 'left')
+            ->join("($subMaterial) m", "m.no_model  = out_celup.no_model AND m.item_type  = other_bon.item_type AND m.kode_warna = other_bon.kode_warna AND m.color = other_bon.warna", 'left')
             ->join('master_material', 'master_material.item_type = other_bon.item_type', 'left')
             ->where('out_celup.id_other_bon IS NOT NULL')
             ->where('master_material.jenis', 'BENANG')
-            ->groupBy('out_celup.id_out_celup, other_bon.id_other_bon, other_bon.no_model, other_bon.item_type, other_bon.kode_warna');
+            ->groupBy('out_celup.id_out_celup, other_bon.id_other_bon, out_celup.no_model, other_bon.item_type, other_bon.kode_warna');
         // Query 2: Untuk data pemasukan biasa dari schedule celup
         $builder2 = $this->db->table('pemasukan')
-            ->select('bon_celup.id_bon, schedule_celup.no_model, schedule_celup.item_type, schedule_celup.kode_warna, schedule_celup.warna, SUM(DISTINCT out_celup.kgs_kirim) AS kgs_kirim, SUM(DISTINCT out_celup.cones_kirim) AS cones_kirim, pemasukan.tgl_masuk, pemasukan.nama_cluster, master_order.foll_up, master_order.no_order, master_order.buyer, master_order.delivery_awal, master_order.delivery_akhir, master_order.unit, m.total_kgs AS kgs_material, out_celup.lot_kirim, bon_celup.no_surat_jalan, bon_celup.tgl_datang, out_celup.l_m_d, SUM(DISTINCT out_celup.gw_kirim) AS gw_kirim, out_celup.harga, bon_celup.keterangan, pemasukan.admin, schedule_celup.po_plus')
+            ->select('bon_celup.id_bon, out_celup.no_model, schedule_celup.item_type, schedule_celup.kode_warna, schedule_celup.warna, SUM(DISTINCT out_celup.kgs_kirim) AS kgs_kirim, SUM(DISTINCT out_celup.cones_kirim) AS cones_kirim, pemasukan.tgl_masuk, pemasukan.nama_cluster, master_order.foll_up, master_order.no_order, master_order.buyer, master_order.delivery_awal, master_order.delivery_akhir, master_order.unit, m.total_kgs AS kgs_material, out_celup.lot_kirim, bon_celup.no_surat_jalan, bon_celup.tgl_datang, out_celup.l_m_d, SUM(DISTINCT out_celup.gw_kirim) AS gw_kirim, out_celup.harga, bon_celup.keterangan, pemasukan.admin, schedule_celup.po_plus')
             ->join('out_celup', 'out_celup.id_out_celup = pemasukan.id_out_celup')
             ->join('schedule_celup', 'schedule_celup.id_celup = out_celup.id_celup')
-            ->join('master_order', 'master_order.no_model = schedule_celup.no_model', 'left')
+            ->join('master_order', 'master_order.no_model = out_celup.no_model', 'left')
             ->join('open_po', 'open_po.no_model = master_order.no_model AND open_po.kode_warna = schedule_celup.kode_warna AND open_po.item_type = schedule_celup.item_type', 'left')
             ->join('bon_celup', 'bon_celup.id_bon = out_celup.id_bon', 'left')
             ->join("($subMaterial) m", "m.no_model  = schedule_celup.no_model AND m.item_type  = schedule_celup.item_type AND m.kode_warna = schedule_celup.kode_warna AND m.color = schedule_celup.warna", 'left')
@@ -220,7 +220,7 @@ class PemasukanModel extends Model
         if (!empty($key)) {
             // builder1 untuk pencarian di kolom other_bon
             $builder1->groupStart()
-                ->like('other_bon.no_model', $key)
+                ->like('out_celup.no_model', $key)
                 ->orLike('other_bon.item_type', $key)
                 ->orLike('other_bon.kode_warna', $key)
                 ->orLike('other_bon.warna', $key)
@@ -230,7 +230,7 @@ class PemasukanModel extends Model
 
             // builder2 untuk pencarian di kolom schedule_celup
             $builder2->groupStart()
-                ->like('schedule_celup.no_model', $key)
+                ->like('out_celup.no_model', $key)
                 ->orLike('schedule_celup.item_type', $key)
                 ->orLike('schedule_celup.kode_warna', $key)
                 ->orLike('schedule_celup.warna', $key)
