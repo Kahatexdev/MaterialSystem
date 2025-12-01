1f355ba488436aefe85c76452076bdc6bb6cc253
ydwhf <yudawhf@gmail.com>
2025-12-01 15:20:51 +0700

    Fix Report Global Benang

--- app/Controllers/ExcelController.php
+++ app/Controllers/ExcelController.php
@@ -2319,8 +2319,9 @@ class ExcelController extends BaseController
             $sheet->setCellValue('X' . $row, isset($item['stock_akhir']) ? number_format($item['stock_akhir'], 2, '.', '') : 0); // stock akhir
 
             // Tagihan GBN dan Jatah Area perhitungan
-            $tagihanGbn = isset($item['kgs']) ? $item['kgs'] + $item['qty_poplus'] - ($item['datang_solid'] + $item['plus_datang_solid'] + $item['stock_awal']) : 0;
-            $jatahArea = isset($item['kgs']) ? $item['kgs'] + $item['qty_poplus'] - $item['pakai_area'] : 0;
+            $tagihanGbn = isset($item['kgs_stock_awal']) ? ($item['kgs_stock_awal'] + $item['datang_solid'] + $item['plus_datang_solid'] + $item['retur_stock'] - $item['kgs'] - $item['qty_poplus']) : 0;
+
+            $jatahArea = isset($item['pakai_area']) ? (($item['pakai_area'] ?? 0) - ($item['retur_pb_area'] ?? 0) - ($item['kgs'] ?? 0) - ($item['qty_poplus'] ?? 0)) : 0;
 
             // Format Tagihan GBN dan Jatah Area
             $sheet->setCellValue('Y' . $row, number_format($tagihanGbn, 2, '.', '')); // tagihan gbn
